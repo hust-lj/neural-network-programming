bbc8f677e773bd80540c43f1f3cf6ece6fe998a3
liujian <liujian@karrytech.com>
2019-07-27 00:19:19 +0800

    Second Commit

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# -
-
 #使用Python进行神经网络编程识别手写图片
 
-#The code mainly provides a neural network programming idea, the code is simple and clear, so that beginners can quickly understand and write their own image recognition code.
+#该代码主要提供了神经网络编程思想，代码简洁明了，使初学者可以快速理解并编写识别手写数字图片的代码。
 
-#The code is based on Python 2.7.
+#该代码基于Python2.7，对应的训练集数据和测试集数据来源网站如下，训练的数据较多，可提取部分数据进行测试
+#训练集http://www.pjreddie.com/media/files/mnist_train.csv
+#测试集 http://www.pjreddie.com/media/files/mnist_test.csv
+
+#后面逐步开发对应的图片处理算法
--- nue.py
+++ nue.py
@@ -1,27 +1,54 @@
+#-*-coding:utf8-*-
 import numpy
 import scipy.special
 
-class neuralNetwork:
+class NeuralNetwork:
     def __init__(self,inputnodes,hiddennodes, outputnodes,learningrate):
         self.inodes = inputnodes
         self.hnodes = hiddennodes
         self.onodes = outputnodes
         self.lr = learningrate
+
+        # 初始化输入节点和隐藏节点间的权重
         self.wih = numpy.random.normal(0.0,pow(self.hnodes,-0.5),(self.hnodes,self.inodes))
+
+        # 初始化隐藏节点和输出节点间的权重
         self.who = numpy.random.normal(0.0,pow(self.onodes,-0.5),(self.onodes,self.hnodes))
+
+        # 初始化生成S（激活）函数：y=1/(1+e^(-x))
         self.activation_function = lambda x:scipy.special.expit(x)
+
+    # 数据训练函数
     def train(self,input_list,target_list):
         inputs = numpy.array(input_list,ndmin=2).T
+
+        # 期望输出数据
         targets = numpy.array(target_list,ndmin=2).T
+
+        # 输入节点数据与输入和隐藏间权重相乘作为隐藏层的输入数据
         hidden_input = numpy.dot(self.wih,inputs)
+
+        # 隐藏层的输入数据作为参数经过S函数后可得隐藏层的输出数据
         hidden_out = self.activation_function(hidden_input)
+
+        # 隐藏层的输出数据与隐藏和输出间权重相乘作为输出层的输入数据
         final_input = numpy.dot(self.who,hidden_out)
+
+        # 输出层的输入数据作为参数经过S函数后可得输出层的输出数据，即最后输出数据
         final_out = self.activation_function(final_input)
+
+        # 计算期望数据与输出数据间的误差
         output_error = targets - final_out
+
+        # 反向将误差值作为隐藏层的输入数据
         hiden_error = numpy.dot(self.who.T,output_error)
+
+        # 更新三个节点间的权重，使输出数据更接近期望数据，根据误差函数的斜率分析进行，新权重公式为：ΔW=lr * E * O₂ * (1- O₂) · O₁,
+        # 其中lr为学习率，E为误差， O₂  为最后输出数据，O₁为上个节点的输出数据
         self.who += self.lr * numpy.dot((output_error*final_out*(1-final_out)),numpy.transpose(hidden_out))
         self.wih += self.lr * numpy.dot((hiden_error * hidden_out * (1-hidden_out)) , numpy.transpose(inputs))
 
+    # 查询函数，根据训练好的模型进行理想数据输出
     def query(self,input_list):
         inputs = numpy.array(input_list,ndmin=2).T
         hidden_input = numpy.dot(self.wih,inputs)
@@ -30,31 +57,48 @@ class neuralNetwork:
         final_out = self.activation_function(final_input)
         return final_out
 
-with open('mnist_train.csv') as f:
-    train_data_list = f.readlines()
-# all_values=data_list[0].split(',')
-# scaled_input = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-# print(scaled_input)
-n=neuralNetwork(784,200,10,0.1)
-for i in range(4):
-    for line in train_data_list:
-        all_values = line.split(',')
-        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        targets = numpy.zeros(10)+0.01
-        targets[int(all_values[0])] = 0.99
-        n.train(inputs,targets)
+if __name__ == '__main__':
+    recognition_rate = input('请输入期望的识别率：')
+    #初始识别率
+    r_rate = 0
 
-with open('mnist_test.csv') as f:
-    test_data_list = f.readlines()
-right_list = []
-for line in test_data_list:
-    all_values = line.split(',')
-    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    out = n.query(inputs)
-    label = numpy.argmax(out)
-    if int(all_values[0]) == label:
-        right_list.append(1)
-    else:
-        right_list.append(0)
 
-print float( right_list.count(1))/float(len(right_list))
+    # 读取训练集数据
+    with open('mnist_train_100.csv') as f:
+        train_data_list = f.readlines()
+    # 读取测试集数据
+    with open('mnist_test_10.csv') as f:
+        test_data_list = f.readlines()
+
+    # 实例化一个神经网络对象，输入数据数值一般根据该图片的像素进行确定
+    n=NeuralNetwork(784,200,10,0.1)
+
+    while r_rate < recognition_rate:
+        for i in range(10):
+            for line in train_data_list:
+                all_values = line.split(',')
+                # 将输入数据转为小于1的形式，方便激活函数的计算
+                inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+
+                # 将对应的期望数据转为对应0.01-0.09的列表
+                targets = numpy.zeros(10)+0.01
+                targets[int(all_values[0])] = 0.99
+
+                # 开始训练模型
+                n.train(inputs,targets)
+
+        #开始测试数据并获取识别率
+        right_list = []
+        for line in test_data_list:
+            all_values = line.split(',')
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            out = n.query(inputs)
+            # 获取该列表中最大数值的索引
+            label = numpy.argmax(out)
+            if int(all_values[0]) == label:
+                right_list.append(1)
+            else:
+                right_list.append(0)
+
+        r_rate = float( right_list.count(1))/float(len(right_list))
+    print r_rate
